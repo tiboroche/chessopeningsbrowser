d01190f9e4210276c5a48c2f363b1511957164a5
Thibault Roche <thibault@diwi.org>
2022-02-27 19:04:41 +0100

    Configuration continues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,60 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        // eslint-disable-next-line max-len
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: "src/<%= pkg.name %>.js",
+        dest: "build/<%= pkg.name %>.min.js",
       },
     },
     copy: {
       main: {
-        files: [{
-          expand: true,
-          cwd: 'node_modules/',
-          src: [ 'chess.js/chess.js', '@chrisoakman/chessboardjs/dist/*.min.*', 'jquery/dist/jquery.min.js'],
-          dest: 'dist/',
-          flatten: true
-        },
-        {
-          expand: true,
-          src: 'build/<%= pkg.name %>.min.js',
-          dest: 'dist/',
-          flatten: true
-        },
-        {
-          expand: true,
-          src: 'src/index.html',
-          dest: 'dist/',
-          flatten: true
-        },{
-          expand: true,
-          src: 'img/**',
-          dest: 'dist'
-        } ]
-      } 
+        files: [
+          {
+            expand: true,
+            cwd: "node_modules/",
+            src: [
+              "chess.js/chess.js",
+              "@chrisoakman/chessboardjs/dist/*.min.*",
+              "jquery/dist/jquery.min.js",
+            ],
+            dest: "dist/",
+            flatten: true,
+          },
+          {
+            expand: true,
+            src: "build/<%= pkg.name %>.min.js",
+            dest: "dist/",
+            flatten: true,
+          },
+          {
+            expand: true,
+            src: "src/index.html",
+            dest: "dist/",
+            flatten: true,
+          },
+          {
+            expand: true,
+            src: "img/**",
+            dest: "dist",
+          },
+        ],
+      },
     },
-    clean: ['dist/']
+    clean: ["dist/"],
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
   // Default task(s).
-  grunt.registerTask('default', ['clean', 'copy', 'uglify']);
-
+  grunt.registerTask("default", ["clean", "copy", "uglify"]);
 };
--- src/chessopeningsbrowser.js
+++ src/chessopeningsbrowser.js
@@ -1,1 +1,10 @@
-var board1 = Chessboard('board1', 'start');
+;(function() {
+  'use strict';
+
+  // the main board
+  // eslint-disable-next-line new-cap
+  const board1 = Chessboard('board1', 'start');
+
+  // load the openings structure
+  // display the buttons
+})();
